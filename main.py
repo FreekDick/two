39fdb44607005a336429120aae9af622f15453c3
Stevan <140596498+StevanGriliano@users.noreply.github.com>
2024-02-13 14:48:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def multiply(n1, n2):
     return n1 * n2
 
 def divide(n1, n2):
-    return n1 / n2
+    return n4 / n6
 
 ##Functions are first-class objects, can be passed around as arguments e.g. int/string/float etc.
 
